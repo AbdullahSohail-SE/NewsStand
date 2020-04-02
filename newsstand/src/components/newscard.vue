d8c9fadb78882a57e6d287641245ff73db826ced
Abdullah Sohail <abdullahsohailofficial123@gmail.com>
2020-04-02 17:03:19 +0500

    reusable card initial

---
+++ newsstand/src/components/newscard.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-card :class="cardSize" flat square>
+          <q-skeleton v-if="!dataFetched" :class="cardSize" square></q-skeleton>
+          <div :class="cardSize" v-else>
+            <q-img :src="newsarticle.urlToImage" height="100%" spinner-color="primary">
+              <div class="absolute-bottom q-img-caption">
+                <h3 class="q-img-caption-title q-img-caption-title-lg">
+                 <a :href="newsarticle.url">{{newsarticle.title}}</a> 
+                </h3>
+                <p class="q-img-caption-desc">{{newsarticle.description}}</p>
+              </div>
+            </q-img>
+          </div>
+  </q-card>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      dataFetched:false
+    }
+  },
+  props:{
+   cardType:{
+      type:String,
+      required:true },
+   article:{
+     required:true}
+  },
+  computed:{
+    newsarticle:function(){
+      console.log(this.article);
+      if(this.article.title){
+        this.datafetched=true;
+        return this.article;
+      }
+    },
+    cardSize:function(){
+      if (this.cardType=="large")
+      return 'height-lg';
+      else if(this.cardType=="medium")
+      return 'height-md';
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+//SASS variables
+  $height-lg:510px;
+  $height-md:250px;
+
+.height-lg{
+  height: $height-lg;
+}
+.height-md{
+  height: $height-md;
+}
+.q-img-caption{
+  background-color: transparent;
+  background-image: linear-gradient(to bottom, transparent, rgba(0,0,0,0.9));
+
+  &-title{
+    
+    font-weight: 500;
+    line-height: normal;
+    margin: 0;
+
+    &-sm{
+      font-size: 18px;
+    }
+    &-md{
+      font-size: 24px;
+    }
+    &-lg{
+      font-size: 36px;
+    }
+  }
+  &-desc{
+    margin-top: 5px;
+  }
+}
+
+</style>
--- newsstand/src/pages/Index.vue
+++ newsstand/src/pages/Index.vue
@@ -6,53 +6,18 @@
         <span class="text-h6 " style="font-weight:500">{{ currentFormatDate }}</span>
       </header>
       <div class="col-7 q-px-xs q-py-lg">
-        <q-card class="card-height-lg" flat square>
-          <q-skeleton v-if="!dataFetched" class="quasar-skeleton-lg" square>{{articles}}</q-skeleton>
-          <div class="quasar-skeleton-lg" v-else>
-            <q-img :src="articles[0].urlToImage" height="100%" spinner-color="primary">
-              <div class="absolute-bottom q-img-caption">
-                <h3 class="q-img-caption-title q-img-caption-title-lg">
-                 <a :href="articles[0].url">{{articles[0].title}}</a> 
-                </h3>
-                <p class="q-img-caption-desc">{{articles[0].description}}</p>
-              </div>
-            </q-img>
-          </div>
-        </q-card>
+        <newscard cardType="large" :article="articles[0]"></newscard>
       </div>
       <div class="col-5 q-px-xs q-py-lg">
-        <q-card class="card-height-md q-mb-sm" flat square>
-          <q-skeleton v-if="!dataFetched" class="quasar-skeleton-md" square>{{articles}}</q-skeleton>
-          <div class="quasar-skeleton-md" v-else>
-            <q-img :src="articles[1].urlToImage" height="100%" spinner-color="primary">
-              <div class="absolute-bottom q-img-caption">
-                <h3 class="q-img-caption-title q-img-caption-title-md">
-                 <a :href="articles[1].url">{{articles[1].title}}</a> 
-                </h3>
-                <p class="q-img-caption-desc">{{articles[1].description}}</p>
-              </div>
-            </q-img>
-          </div>
-        </q-card>
-        <q-card class="card-height-md" flat square>
-          <q-skeleton v-if="!dataFetched" class="quasar-skeleton-md" square>{{articles}}</q-skeleton>
-          <div class="quasar-skeleton-md" v-else>
-            <q-img :src="articles[2].urlToImage" height="100%" spinner-color="primary">
-              <div class="absolute-bottom q-img-caption">
-                <h3 class="q-img-caption-title q-img-caption-title-md">
-                 <a :href="articles[2].url">{{articles[2].title}}</a> 
-                </h3>
-                <p class="q-img-caption-desc">{{articles[2].description}}</p>
-              </div>
-            </q-img>
-          </div>
-        </q-card>
+        <newscard cardType="medium" :article="articles[1]"></newscard>
+        <newscard cardType="medium" :article="articles[2]"></newscard>
       </div>
     </section>
   </q-page>
 </template>
 
 <script>
+import newscard from '../components/newscard'
 export default {
   data(){
     return{
@@ -67,21 +32,18 @@ export default {
     },
     articles:function(){
       var articles=this.$store.getters.getTopAroundTheGlobe;
-      if(articles.length>0)
-      {
-        this.dataFetched=true;
-        return articles;
-      }
-        
+      console.log(articles);
+      return articles;
     }
+  },
+  components:{
+    newscard
   }
 }
 </script>
 <style lang="scss" scoped>
 
-//SASS variables
-  $height-lg:510px;
-  $height-md:250px;
+
 
 .app-headings{
   font-size:1.5rem;
@@ -93,46 +55,12 @@ export default {
 .margin-0{
   margin: 0;
 }
-.card-height-lg{
-  min-height: $height-lg;
-}
-.card-height-md{
-  min-height: $height-md;
-}
+
 .text-h6{
   font-weight:normal;
 }
 .header{
   margin: 0 auto;
 }
-.quasar-skeleton-lg{
-  height: $height-lg;
-}
-.quasar-skeleton-md{
-  height: $height-md;
-}
-.q-img-caption{
-  background-color: transparent;
-  background-image: linear-gradient(to bottom, transparent, rgba(0,0,0,0.9));
 
-  &-title{
-    
-    font-weight: 500;
-    line-height: normal;
-    margin: 0;
-
-    &-sm{
-      font-size: 18px;
-    }
-    &-md{
-      font-size: 24px;
-    }
-    &-lg{
-      font-size: 36px;
-    }
-  }
-  &-desc{
-    margin-top: 5px;
-  }
-}
 </style>
