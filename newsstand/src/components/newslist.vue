5b7278b9f2d79253dbc752bf2e4baeca0cc48478
Abdullah Sohail <abdullahsohailofficial123@gmail.com>
2020-04-03 22:26:09 +0500

    newslist version 1

---
+++ newsstand/src/components/newslist.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-list class="quasar-list" separator>
+            <q-skeleton v-if="!dataFetched" class="q-mt-md " style="min-height:500px"  square><span class="ellipsis">{{newsarticles}}</span></q-skeleton>
+            <q-item v-for="article in newsarticles" :key="article.url" v-else>
+                <q-item-section class="text-weight-600 hoverunderline">{{article.title}}</q-item-section>
+            </q-item>
+  </q-list>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      dataFetched:false
+    }
+  },
+  props:{
+    number:{
+      type:Number,
+      default:10
+    },
+    articles:{
+      type:Array,
+      required:true
+    }
+
+  },
+  computed:{
+    newsarticles:function(){
+      if(this.articles.length>0)
+      {
+        this.dataFetched=true;
+        return this.articles.slice(0,this.number);
+      }
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.quasar-list > div{
+  padding-left: 0;
+  padding-right: 0;
+}
+
+
+</style>
--- newsstand/src/pages/Index.vue
+++ newsstand/src/pages/Index.vue
@@ -2,57 +2,22 @@
   <q-page padding>
     <section class="row items-center q-mt-lg">
       <header class="row items-center justify-between col-12 header q-px-sm ">
-        <h1 class="q-ma-none app-headings">Top Around The Globe</h1>
-        <span class="text-h6 " style="font-weight:500">{{ currentFormatDate }}</span>
+        <h1 class="q-ma-none app-headings text-subtle-grey">Top Around The Globe</h1>
+        <span class="text-h6 text-subtle-grey" style="font-weight:500">{{ currentFormatDate }}</span>
       </header>
       <div class="col-7 q-px-xs q-py-lg">
-        <newscard cardType="large" :article="articles[0]" :captionsize="'large'"></newscard>
+        <newscard cardType="large" :article="topArticles[0]" :captionsize="'large'"></newscard>
       </div>
       <div class="col-5 q-px-xs q-py-lg">
-        <newscard class="q-mb-sm" cardType="medium" :article="articles[1]" :captionsize="'small'"></newscard>
-        <newscard cardType="medium" :article="articles[2]" :captionsize="'small'"></newscard>
+        <newscard class="q-mb-sm" cardType="medium" :article="topArticles[1]" :captionsize="'small'"></newscard>
+        <newscard cardType="medium" :article="topArticles[2]" :captionsize="'small'"></newscard>
       </div>
       <div class="row col-12" >
         <div class="col-12">
           <q-item-label class="q-px-none" header>More News</q-item-label>
         </div>
         <div class="col-4 q-pr-md">
-          <q-list class="quasar-list" separator="" >
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
-            </q-item>
-           
-          </q-list>
+          <newslist :articles="topArticles.slice(3)"></newslist>
         </div>
          <div class="col-5 bg-red">
            <q-card class="q-py-md" square flat>
@@ -92,6 +57,7 @@
 
 <script>
 import newscard from '../components/newscard'
+import newslist from '../components/newslist'
 export default {
   data(){
     return{
@@ -104,41 +70,18 @@ export default {
       var day=unformated.toDateString().split(' ')[0];
       return unformated.toDateString().replace(day,`${day},`);   
     },
-    articles:function(){
+    topArticles:function(){
        var articles=this.$store.getters.getTopAroundTheGlobe;
       return articles;
     }
   },
   components:{
-    newscard
+    newscard,
+    newslist
   }
 }
 </script>
 <style lang="scss" scoped>
 
-
-
-.app-headings{
-  font-size:1.5rem;
-  font-weight:500;
-  display: inline;
-  line-height: 1rem;
-}
-
-.quasar-list > div{
-  padding-left: 0;
-  padding-right: 0;
-}
-
-.normal-line-height{
-line-height: normal;
-}
-
-.text-h6{
-  font-weight:normal;
-}
-.header{
-  margin: 0 auto;
-}
 
 </style>
