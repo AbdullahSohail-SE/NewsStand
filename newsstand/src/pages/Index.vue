f307f57e245ef8d0151ce28a6381abb672d795fd
Abdullah Sohail <abdullahsohailofficial123@gmail.com>
2020-04-03 14:10:26 +0500

    Section Template

--- newsstand/src/pages/Index.vue
+++ newsstand/src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <section class="row items-center q-mt-lg">
       <header class="row items-center justify-between col-12 header q-px-sm ">
-        <h1 class="margin-0 app-headings">Top Around The Globe</h1>
+        <h1 class="q-ma-none app-headings">Top Around The Globe</h1>
         <span class="text-h6 " style="font-weight:500">{{ currentFormatDate }}</span>
       </header>
       <div class="col-7 q-px-xs q-py-lg">
@@ -10,7 +10,81 @@
       </div>
       <div class="col-5 q-px-xs q-py-lg">
         <newscard class="q-mb-sm" cardType="medium" :article="articles[1]" :captionsize="'small'"></newscard>
-        <newscard cardType="medium" :article="articles[3]" :captionsize="'small'"></newscard>
+        <newscard cardType="medium" :article="articles[2]" :captionsize="'small'"></newscard>
+      </div>
+      <div class="row col-12" >
+        <div class="col-12">
+          <q-item-label class="q-px-none" header>More News</q-item-label>
+        </div>
+        <div class="col-4 q-pr-md">
+          <q-list class="quasar-list" separator="" >
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+            <q-item>
+                <q-item-section class="text-weight-600 hoverunderline">Lorem ipsum dolor sit amet consectetur.</q-item-section>
+            </q-item>
+           
+          </q-list>
+        </div>
+         <div class="col-5 bg-red">
+           <q-card class="q-py-md" square flat>
+             <q-img class="bg-red" spinner-color="primary" height="330px"></q-img>
+             <q-card-section class="q-px-none">
+               <h6 class="q-ma-none q-pa-none normal-line-height">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
+              <p class="q-my-sm text-body-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi sit animi perferendis quia optio provident doloribus aliquam repellendus ipsam commodi.</p>
+             </q-card-section>
+             <q-card-section class="q-pa-none row justify-between">
+               <span class="q-py-none text-subtle-grey">NyTimes.com</span>
+               <span class="q-py-none text-subtle-grey">22 March 2109</span>
+             </q-card-section>
+           </q-card>
+          </div>
+        <div class="col-3 q-px-sm column">
+            <div>
+              <q-card class="q-py-md" square flat>
+                <q-img spinner-color="primary" class="bg-red" height="150px"></q-img>
+                <q-card-section class="q-pa-none">
+                  <p class="q-my-sm text-body-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsum..</p>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div>
+              <q-card class="q-py-md" square flat>
+                <q-img spinner-color="primary" class="bg-red" height="150px"></q-img>
+                <q-card-section class="q-pa-none">
+                  <p class="q-my-sm text-body-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsum..</p>
+                </q-card-section>
+              </q-card>
+            </div>
+        </div>
       </div>
     </section>
   </q-page>
@@ -51,8 +125,13 @@ export default {
   line-height: 1rem;
 }
 
-.margin-0{
-  margin: 0;
+.quasar-list > div{
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.normal-line-height{
+line-height: normal;
 }
 
 .text-h6{
