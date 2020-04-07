398cc9352c81372c1e2347d0aca8c0ba223110c4
Abdullah Sohail <abdullahsohailofficial123@gmail.com>
2020-04-07 17:11:39 +0500

    Large Screen Responsiveness

--- newsstand/src/layouts/AppHeader.vue
+++ newsstand/src/layouts/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <q-header>
-    <q-toolbar class="bg-white text-black">
-      
+    <q-toolbar class=" row bg-white text-black">
+      <div class="col-2 row items-center no-wrap">
       <q-avatar font-size="42px" size="54px" text-color="teal">
         <i class="fab fa-hubspot"></i>
       </q-avatar>
@@ -9,12 +9,14 @@
       <q-toolbar-title class="title q-mr-lg" shrink>
         NEWSSTAND
       </q-toolbar-title>
+      </div>
 
-      <q-tabs shrink align="left">
-        <q-route-tab label="Latest" to="/" exact ></q-route-tab>
-        <q-route-tab label="General" to="/general" exact></q-route-tab>
-        <q-route-tab label="Science" to="/science" exact></q-route-tab>
-        <q-route-tab label="Business" to="/science" exact></q-route-tab>
+      <div class="col-6 row items-center no-wrap">
+      <q-tabs>
+        <q-route-tab class="resp-tab"  label="Latest" to="/" exact ></q-route-tab>
+        <q-route-tab class="resp-tab"  label="General" to="/general" exact></q-route-tab>
+        <q-route-tab class="resp-tab"  label="Science" to="/science" exact></q-route-tab>
+        <q-route-tab class="resp-tab"  label="Business" to="/science" exact></q-route-tab>
       </q-tabs>
       <q-btn-dropdown menu-anchor="bottom middle" menu-self="top middle" content-class="menu"  flat dense dropdown-icon="none" class="icon_catogary">
           <div class="row q-pa-md text-weight-medium" style="width:430px">
@@ -59,29 +61,33 @@
             </div>
           </div>
       </q-btn-dropdown>
-      
-
-      <q-input dense outlined placeholder="Search News" v-model="searchQuery" input-class="text-left" class="q-ml-md">
+      <q-input  dense outlined placeholder="Search News" v-model="searchQuery" input-class="text-left" class="q-ml-md ">
           <template v-slot:append>
             <q-icon name="search" />
           </template>
       </q-input>
+      </div>
+      
+      
       <q-space></q-space>
+
+      <div class="col-4 row items-center justify-end">
       <q-tabs shrink inline-label >
-        <q-route-tab dense icon="bookmarks" label="Saved" to="/readinglist" exact >
-        <q-badge style="right:-20px !important" color="red" floating>2</q-badge>
+        <q-route-tab  class="resp-tab"  dense icon="bookmarks" label="Saved" to="/readinglist" exact >
+        <q-badge   color="red" class="resp-badge" >2</q-badge>
         </q-route-tab>
       
-        <q-route-tab alert icon="notifications" to="/notifications">
-        <q-badge color="red" floating>5</q-badge>
+        <q-route-tab  class="resp-tab" icon="notifications" to="/notifications">
+        <q-badge  color="red" class="resp-badge">5</q-badge>
         </q-route-tab>
         <q-route-tab to="/user">
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/avatar.png">
+          <q-avatar >
+            <img style="z-index:10000" src="https://cdn.quasar.dev/img/avatar.png">
           </q-avatar>
-          <span class="text-weight-medium q-ml-sm">Abdullah Sohail</span>
+          <span  class="text-weight-medium q-ml-sm">Abdullah Sohail</span>
         </q-route-tab>
       </q-tabs>
+      </div>
     </q-toolbar>
     
   </q-header>
@@ -108,5 +114,17 @@ content: url('~assets/catogaries.svg');
     transform:translateX(-50%) translateY(-50%);
     
 }
+@media only screen and (min-width:"1440px") {
+  .resp-tab{
+    width: 5rem;
+    height: 100%;
+    position: relative;
+    
+  }
+}
+.resp-badge{
+    transform: translate(4px,-10px);;
+  }
+
 
 </style>
