ebd236d1b33719b8b6e6b857184ad95d472008af
Abdullah Sohail <abdullahsohailofficial123@gmail.com>
2020-04-12 14:55:35 +0500

    Initial

--- newsstand/src/pages/General.vue
+++ newsstand/src/pages/General.vue
@@ -1,11 +1,20 @@
 <template>
-  
+  <q-page  padding>
+    <section class="row">
+    <header class="col-12 q-my-lg items-center justify-between">
+    <h1 class="q-ma-none app-headings text-subtle-grey">Top Around The Globe</h1>
+    </header>
+    
+    </section>
+  </q-page>  
 </template>
 <script>
 export default {
   name:"general"
 }
 </script>
-<style scoped>
-
+<style lang="scss" scoped>
+.viewport-height{
+  height: 100vh;
+}
 </style>
