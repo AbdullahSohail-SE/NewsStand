3e5c69397f11eb0ffe222307f3f4f4025207f904
Abdullah Sohail <abdullahsohailofficial123@gmail.com>
2020-05-05 03:59:03 +0500

    Footer responsive

--- newsstand/src/layouts/AppFooter.vue
+++ newsstand/src/layouts/AppFooter.vue
@@ -1,7 +1,9 @@
 <template>
-  <q-footer>
+<div>
+  <!-- desktop-version -->
+  <q-footer class="desktop-only">
     <div class="row q-pa-lg">
-      <div class="col-4">
+      <div class="col-4 col-md-4 col-lg-4 col-sm-12 col-xs-12 desktop-only">
         <q-list dense>
           <q-item-label header class="text-white">Sections</q-item-label>
           <q-item>
@@ -18,7 +20,7 @@
           </q-item>
         </q-list>
       </div>
-      <div class="col-4">
+      <div class="col-4 col-md-4 col-lg-4 col-sm-12 col-xs-12">
           <q-list dense>
           <q-item-label header class="text-white">About</q-item-label>
           <q-item>
@@ -41,7 +43,7 @@
           </q-item>
         </q-list>
       </div>
-      <div class="col-4">
+      <div class="col-4 col-md-4 col-lg-4 col-sm-12 col-xs-12">
         <q-item-label header class="text-white">Connect with us</q-item-label>
           <q-item>
               <a href="https://www.facebook.com" class="anchor q-mr-sm" style="font-size:36px">
@@ -73,6 +75,67 @@
       <span class="">Copyright © 2020 NEWSSTAND News Internet Ventures.k</span>
     </div>
   </q-footer>
+  <!-- -- -->
+
+  <!-- mobile-version -->
+  <q-footer class="mobile-only">
+    <div class="row q-pa-lg text-center">
+       
+      <div class="col-12">
+          <q-list class="row items-center justify-between" dense>
+          <q-item class="col-6">
+          <router-link class="anchor" tag="a" to="#dummy">Contact us</router-link>
+          </q-item>
+          <q-item class="col-6">
+          <router-link class="anchor" tag="a" to="#dummy">Feedback</router-link>
+          </q-item>
+          <q-item class="col-6">
+          <router-link class="anchor" tag="a" to="#dummy">Terms of use</router-link>
+          </q-item>
+          <q-item class="col-6">
+          <router-link class="anchor" tag="a" to="#dummy">Privacy policy</router-link>
+          </q-item>
+          <q-item class="col-6">
+          <router-link class="anchor" tag="a" to="#dummy">Advertising</router-link>
+          </q-item>
+          <q-item class="col-6">
+          <router-link class="anchor" tag="a" to="#dummy">Parental Guidance</router-link>
+          </q-item>
+        </q-list>
+      </div>
+      <div class="col-4 q-mt-sm col-md-4 col-lg-4 col-sm-12 col-xs-12">
+        <q-item-label header class="text-white"><q-icon style="margin-top:-4px" name="fas fa-heart"></q-icon> Like this? Hit that star button on github </q-item-label>
+          <q-item>
+              <a href="https://www.facebook.com" class="anchor q-mr-sm" style="font-size:36px">
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="https://github.com/AbdullahSohail-SE" class="anchor q-mx-sm" style="font-size:36px">
+                <i  class="fab fa-github"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/abdullahsohailned/?originalSubdomain=pk" class="anchor q-mx-sm" style="font-size:36px">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+              <a class="anchor q-mx-sm" style="font-size:36px">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a class="anchor q-mx-sm" style="font-size:36px">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a class="anchor q-mx-sm" style="font-size:36px">
+                <i class="fab fa-youtube"></i>
+              </a>
+          </q-item>
+          <div class="q-ml-md q-mt-md">
+            <span>* This website is for purely educational purposes and not for commercial usage.I have used the services of <a href="https://newsapi.org/" target="blank" class="anchor a-external"> News API <i class="fas fa-external-link-alt"></i></a> to serve content on this site</span>
+          </div>
+      </div>
+    </div>
+    
+    <div class="bg-white text-primary text-bold text-center q-pa-sm">
+      <span class="">Copyright © 2020 NEWSSTAND News Internet Ventures.k</span>
+    </div>
+  </q-footer>
+</div>
 </template>
 <script>
 
